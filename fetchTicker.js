c28a7eed6242d9ba05eb518c0cc6c1a581e8ddf7
Znort 987 <znort987@yahoo.com>
2012-07-09 17:46:05 +0200

    "cached fetch", inspiration: btharper's code

--- fetchTicker.js
+++ fetchTicker.js
@@ -2,26 +2,62 @@
 // Google Docs JS Macro to process fetch a GLBSE ticker
 // Written by znort987@yahoo.com, tips to: 1ZnortsoStC1zSTXbW6CUtkvqew8czMMG
 
+function dontHammerGLBSE()
+{
+    var cache = CacheService.getPublicCache()
+    var now = (new Date()).getTime()
+    var last = cache.get('last')
+
+    if (null != last) {
+        var lastTime = parseInt(last)
+        var elapsed = now - lastTime
+        var waitFor = 5 - elapsed
+        if (0<waitFor)
+            Utilities.sleep(waitFor)
+    }
+    
+    cache.put('last', String(now))
+}
+
+function fetchCached(url)
+{
+    var publicCache = CacheService.getPublicCache()
+    var cached = publicCache.get(url)
+
+    if (null == cached) {
+    
+        dontHammerGLBSE()
+        
+        var response = UrlFetchApp.fetch(url)
+        if ('undefined' != typeof(response)) {
+            var code = response.getResponseCode()
+            if (code<300) {
+                var fiveMin = 5 * 60
+                cached = response.getContentText()
+                publicCache.put(url, cached, fiveMin)
+            }
+        }
+    }
+
+    return cached
+}
+
 function fetchTicker(
     tickerName,     // e.g. :  'COGNITIVE'
     fieldName       // e.g. :  'ask'  (see code for full list of args)
 )
 {
     // Fetch live data from GLBSE API
-    var json = UrlFetchApp.fetch("https://glbse.com/api/asset/" + tickerName)
-    if ('undefined' == typeof(json))
+    var r = fetchCached("https://glbse.com/api/asset/" + tickerName)
+    if ('undefined' == typeof(r))
         return 'No data for ticker ' + tickerName + ' from GLBSE'
         
-    json = json.getContentText()
-    if (json.length<=0)
-        return 'GLBSE returned invalid data for ticker ' + tickerName
-        
-    json = Utilities.jsonParse(json)
-    if ('undefined' == typeof(json))
+    r = Utilities.jsonParse(r)
+    if ('undefined' == typeof(r))
         return 'Malformed JSON data returned by GLBLSE for ticker ' + tickerName
         
-    json = json[fieldName]
-    if ('undefined' == typeof(json))
+    r = r[fieldName]
+    if ('undefined' == typeof(r))
         return 'Unknown field ' + fieldName + ' for ticker ' + tickerName
 
     /*
@@ -42,6 +78,6 @@ function fetchTicker(
         }
     */
 
-    return json
+    return r
 }
 
